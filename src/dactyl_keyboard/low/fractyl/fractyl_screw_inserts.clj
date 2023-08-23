f654a01862c23dbc6c271590febbeb4ad00cc237
Kweku <kwe.boakye@googlemail.com>
2023-08-23 23:59:08 +0100

    fix fractyl bottom plate screw positions

--- src/dactyl_keyboard/low/fractyl/fractyl_screw_inserts.clj
+++ src/dactyl_keyboard/low/fractyl/fractyl_screw_inserts.clj
@@ -8,11 +8,15 @@
    [dactyl-keyboard.switch-hole :refer [mount-width]]
    [dactyl-keyboard.low.case-low-polyhedron-functions :refer :all]))
 
-(defn fractyl-screw-insert-shape [bottom-radius top-radius height]
+(defn fractyl-screw-insert-shape [bottom-radius top-radius height &{:keys [top-sphere]
+                                                                    :or {top-sphere true}}]
   (union
    (->> (binding [*fn* 30]
           (cylinder [bottom-radius top-radius] height)))
-   (translate [0 0 (/ height 2)] (->> (binding [*fn* 30] (sphere top-radius))))))
+   (cond top-sphere
+         (translate [0 0 (/ height 2)]
+                    (->> (binding [*fn* 30] (sphere top-radius)))))
+   ))
 
 ;; (defn screw-insert [column row bottom-radius top-radius height offset]
 ;;   (let [shift-right   (= column lastcol)
@@ -27,28 +31,30 @@
 ;;          (translate (map + offset [(first position) (second position) (/ height 2)])))))
 ; Hole Diameter C: 4.1-4.4
 
-(defn fractyl-screw-insert [wall-position bottom-radius top-radius height &{:keys [offset] :or {offset [0 0 0]}}]
+(defn fractyl-screw-insert [wall-position bottom-radius top-radius height 
+                            &{:keys [offset top-sphere] :or {offset [0 0 0] top-sphere true}}]
   (let [wall-inner-floor-position (:wall-locate-2-bottom-floor (calculate-control-points wall-position))]
-    (->> (fractyl-screw-insert-shape bottom-radius top-radius height)
+    (->> (fractyl-screw-insert-shape bottom-radius top-radius height :top-sphere top-sphere)
        (translate (mapv + offset wall-inner-floor-position [0 0 (/ height 2)]))))
   )
 (def fractyl-screw-insert-bottom-radius (/ 4.4 2))
+(def fractyl-screw-insert-outer-bottom-radius (+ fractyl-screw-insert-bottom-radius 1.65))
 (def fractyl-screw-insert-top-radius (/ 4.4 2))
 (def fractyl-screw-insert-height 4)
 
-(defn fractyl-screw-insert-all-shapes [bottom-radius top-radius height]
-  (let [left-section-top-left   (fractyl-screw-insert (tps-65-wall-position :tl :north-west) bottom-radius top-radius height :offset [(/ bottom-radius 1.5) (/ bottom-radius -1) 0])
+(defn fractyl-screw-insert-all-shapes [bottom-radius top-radius height &{:keys [top-sphere] :or {top-sphere true}}]
+  (let [left-section-top-left   (fractyl-screw-insert (tps-65-wall-position :tl :north-west) bottom-radius top-radius height :offset [(/ fractyl-screw-insert-outer-bottom-radius 1.5) (/ fractyl-screw-insert-outer-bottom-radius -1) 0] :top-sphere top-sphere)
         ;left-section-bottom (screw-insert 0 lastrow   bottom-radius top-radius height [-50 7 0]) 
-        left-section-top-right  (fractyl-screw-insert (tps-65-wall-position :tr :north) bottom-radius top-radius height :offset [(* 3 (- bottom-radius)) (/ bottom-radius -3)  0])
-        left-section-top-mid  (fractyl-screw-insert (tps-65-wall-position :tm :north) bottom-radius top-radius height )
-        left-section-left-mid  (fractyl-screw-insert (tps-65-wall-position :tl-lm :west) bottom-radius top-radius height)
-        left-section-bm (fractyl-screw-insert (tps-65-wall-position :bm :south) bottom-radius top-radius height :offset [0 (/ bottom-radius 4) 0])
+        left-section-top-right  (fractyl-screw-insert (tps-65-wall-position :tr :north) bottom-radius top-radius height :offset [(* 3 (- fractyl-screw-insert-outer-bottom-radius)) (/ fractyl-screw-insert-outer-bottom-radius -3)  0] :top-sphere top-sphere)
+        left-section-top-mid  (fractyl-screw-insert (tps-65-wall-position :tm :north) bottom-radius top-radius height :top-sphere top-sphere)
+        left-section-left-mid  (fractyl-screw-insert (tps-65-wall-position :tl-lm :west) bottom-radius top-radius height :top-sphere top-sphere)
+        left-section-bm (fractyl-screw-insert (tps-65-wall-position :bm :south) bottom-radius top-radius height :offset [0 (/ fractyl-screw-insert-outer-bottom-radius 4) 0] :top-sphere top-sphere)
         ;thumb-bottom-left  (fractyl-screw-insert (tps-65-wall-position :tl :north-east) bottom-radius top-radius height)
         ;thumb-bottom-right  (fractyl-screw-insert (tps-65-wall-position :tl :north-east) bottom-radius top-radius height)
-        top-mid  (fractyl-screw-insert (key-wall-position 2 0 0 1 :tm ) bottom-radius top-radius height :offset [0 (/ bottom-radius 2) 0])
+        top-mid  (fractyl-screw-insert (key-wall-position 2 0 0 1 :tm) bottom-radius top-radius height :offset [0 0 0] :top-sphere top-sphere)
         top-right  (fractyl-screw-insert (key-wall-position lastcol 0 1 1 :tr) bottom-radius top-radius height
-                                         :offset [(+ (/ bottom-radius -1.5) -1) (+ (/ bottom-radius -1.5) -1) 0])
-        bottom-right  (fractyl-screw-insert (key-wall-position lastcol cornerrow 1 -1 :br :slant :no-slant) bottom-radius top-radius height :offset [(/ bottom-radius -2) (+ (/ bottom-radius 2) 0.5) 0]) ]
+                                         :offset [(+ (/ bottom-radius -1.5) -1) (+ (/ fractyl-screw-insert-outer-bottom-radius -1.5) -1) 0] :top-sphere top-sphere)
+        bottom-right  (fractyl-screw-insert (key-wall-position lastcol cornerrow 1 -1 :br :slant :no-slant) bottom-radius top-radius height :offset [(/ fractyl-screw-insert-outer-bottom-radius -2) (+ (/ fractyl-screw-insert-outer-bottom-radius 2) 0.5) 0] :top-sphere top-sphere) ]
     (union
      left-section-bm
      left-section-top-left
@@ -61,7 +67,7 @@
      top-right
      bottom-right)))
 
-(def fractyl-screw-insert-holes  (fractyl-screw-insert-all-shapes fractyl-screw-insert-bottom-radius fractyl-screw-insert-top-radius fractyl-screw-insert-height))
+(def fractyl-screw-insert-holes  (fractyl-screw-insert-all-shapes fractyl-screw-insert-bottom-radius fractyl-screw-insert-top-radius fractyl-screw-insert-height :top-sphere false))
 
 ; Wall Thickness W:\t1.65
 (def fractyl-screw-insert-outers (fractyl-screw-insert-all-shapes (+ fractyl-screw-insert-bottom-radius 1.65) (+ fractyl-screw-insert-top-radius 1.65) (+ fractyl-screw-insert-height 1.5)))
